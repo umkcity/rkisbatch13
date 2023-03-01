24a941185c457ebb7aa1597a7bbafbc7c1a54fa2
Fayaz <fana.fayaz@gmail.com>
2023-03-01 06:44:36 +0530

    Created java program to add two numbers

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition
+{
+	public static void main(String args[])
+	{
+		int num1,num2,res;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter num1 value");
+		num1=s.nextInt();
+		System.out.println("Enter num2 vlue");
+		num2=s.nextInt();
+		res=num1+num2;
+		System.out.println("The result is "+res);
+	}
+}
