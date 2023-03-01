abbc70e84bf90922f761d43ec1acb6c04e40c51a
Fayaz <fana.fayaz@gmail.com>
2023-03-01 07:15:33 +0530

    Changed num1& num2 to n1 & n2

--- Addition.java
+++ Addition.java
@@ -2,13 +2,13 @@ class Addition
 {
 	public static void main(String args[])
 	{
-		int num1,num2,res;
+		int n1,n2,res;
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter num1 value");
-		num1=s.nextInt();
-		System.out.println("Enter num2 vlue");
-		num2=s.nextInt();
-		res=num1+num2;
+		System.out.println("Enter n1 value");
+		n1=s.nextInt();
+		System.out.println("Enter n2 vlue");
+		n2=s.nextInt();
+		res=n1+n2;
 		System.out.println("The result is "+res);
 	}
 }
