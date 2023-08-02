c94ec73c237fd6a58f64c675d1f779d3cb06c561
Fayaz <fana.fayaz@gmail.com>
2023-08-02 07:23:37 +0530

    removed extra sop statements

--- Addition.java
+++ Addition.java
@@ -12,12 +12,5 @@ class Addition
 		System.out.println("The result is "+res);
 		res=numeber1*number2;
 		System.out.println("The result od product is : "+res);
-		System.out.println("This is first line");
-		System.out.println("This is second line");
-		System.out.println("This is third line");
-		System.out.println("This is fourth line");
-		System.out.println("This is fifth line");
-		System.out.println("This is sixth line");
-		System.out.println("This is seventh line");
 	}
 }
